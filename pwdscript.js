fb4047aca03fdb9dc2b43ff06ba2c846a9c23b9f
Adil Khan <40705470+adilkhan4@users.noreply.github.com>
2019-07-24 15:58:56 +0530

    Add files via upload

--- pwdscript.js
+++ pwdscript.js
@@ -1,7 +1,7 @@
 function check(form)
 {
 
-if(form.userid.value == "a" && form.pwd.value == "a")
+if(form.userid.value == "Adil khan" && form.pwd.value == "Adil@1004")
 {
     window.location.replace('https://drive.google.com/file/d/16GbQ4UAVfW_6k1JyznGq9dEXWdcId-Wt/preview');
   }
